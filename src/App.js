862ff0c395fd993d01df1db3d5240f4d35323c00
iamasee <iamkmasee@gmail.com>
2020-12-03 20:01:39 -0500

    added modernizr

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./styles.css";
 import Media from "react-media";
 import { Switch, Route } from 'react-router-dom';
 
+
 function TouchView() {
   return (
     <div className="page touch">
@@ -22,41 +23,70 @@ function DefaultView() {
 }
 
 
+function WithMediaQueries() {
+  return (
+    <Media
+      queries={{
+        pointer: "(any-pointer: fine)",
+        noPointer: "(any-pointer: coarse)",
+        none: "(any-pointer: none)"
+      }}
+    >
+      {(matches) => {
+
+        let View = DefaultView;
+
+        if (matches.pointer) {
+          View = DefaultView;
+        }
+        else if (matches.pointer && matches.noPointer) {
+          View = DefaultView;
+        }
+        else if (matches.noPointer) {
+          View = TouchView;
+        }
+        else {
+          View = DefaultView;
+        }
+
+
+        return <Fragment>
+          <View />
+        </Fragment>
+      }}
+    </Media>
+  )
+}
+
+
+function WithModernizr() {
+  let View = DefaultView;
+  if (window) {
+    const { Modernizr } = window;
+    if (Modernizr) {
+      if (Modernizr.pointerevents && Modernizr.touchevents) {
+        View = DefaultView;
+      }
+      else if (Modernizr.touchevents) {
+        View = TouchView;
+      }
+    }
+  }
+  return (
+    <View />
+  )
+}
+
 
 export default function App() {
+  console.log(window.Modernizr);
+
   return (
     <div className="App">
-      <Media
-        queries={{
-          pointer: "(any-pointer: fine)",
-          noPointer: "(any-pointer: coarse)",
-          none: "(any-pointer: none)"
-        }}
-      >
-        {(matches) => {
-
-          let View = Fragment;
-
-          if (matches.pointer) {
-            View = DefaultView;
-          }
-          else if (matches.pointer && matches.noPointer) {
-            View = DefaultView;
-          }
-          else if (matches.noPointer) {
-            View = TouchView;
-          }
-          else {
-            View = DefaultView;
-          }
-
-          return <Fragment>
-            <Switch>
-              <Route path="/" component={View} />
-            </Switch>
-          </Fragment>
-        }}
-      </Media>
+      <Switch>
+        <Route path="/" component={WithMediaQueries} />
+        <Route path="/mod" component={WithModernizr} />
+      </Switch>
     </div>
   );
 }
