3fdd8108afc8496f38ad93fc0f2715be0dec2f38
iamasee <iamkmasee@gmail.com>
2020-12-04 19:21:50 -0500

    poly fill 1e9

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 /* polyfills.js */
 
 import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/ie9';
 import 'core-js/features/array/find';
 import 'core-js/features/array/includes';
 import 'core-js/features/number/is-nan';
