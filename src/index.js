6b313ac52b156eaae088031fb6246f3ef0058e84
iamasee <iamkmasee@gmail.com>
2020-12-04 19:22:29 -0500

    ie stable

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
 import 'core-js/features/array/find';
 import 'core-js/features/array/includes';
 import 'core-js/features/number/is-nan';
