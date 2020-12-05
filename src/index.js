2205490799bfe63ad007ad4ffd0574d92bcbfaeb
iamasee <iamkmasee@gmail.com>
2020-12-04 19:13:06 -0500

    polyfill added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+/* polyfills.js */
+
+import 'react-app-polyfill/ie11';
+import 'core-js/features/array/find';
+import 'core-js/features/array/includes';
+import 'core-js/features/number/is-nan';
+import 'array.prototype.fill'
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
